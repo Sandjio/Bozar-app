211d9393544b0cda88157dcad18ce47fdd961eab
Sandjio <emmasandjio@gmail.com>
2022-10-25 01:03:44 +0100

    I added the hero section and some bootsrap icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
+        lightBlue:'#7160de'
 
       },
     },
