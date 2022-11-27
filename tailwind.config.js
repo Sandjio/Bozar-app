c7f233048a4d3e8ef32131c369dfbaf2fc3b2979
Sandjio <emmasandjio@gmail.com>
2022-11-27 16:25:44 +0100

    adjusted the styling of the login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*index.html"],
+  content: [
+    './*index.html',
+    './html/*.html',
+  ],
   theme: {
     extend: {
       colors: {
